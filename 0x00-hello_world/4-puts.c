747cb38adcc5ab214603691b9c1c28b0c2f6c48b
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-03-17 00:03:26 +0100

    first c code in ALX

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
 
-/* Description
-This main function prints a string 
-*/
 
 int main(void)
 {
+/*
+Description
+This main function prints a string 
+*/
 puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
