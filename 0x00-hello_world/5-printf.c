300737ae48fd871dcc6323f82e507bb0946d7468
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-03-17 00:56:25 +0100

    zb

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
 *5-printf-printline
-*main-print and return
-*return-0 successeful
+*main - print and return
+*return - 0 successeful
 */
 
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art, ");
-return(0);
+printf("with proper grammar, but the outcome is a piece of art,/n");
+return (0);
 }
