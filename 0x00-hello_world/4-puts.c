70489dbb4f0e63dfcd45fcb92e7374c14e64295a
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-03-17 00:15:46 +0100

    7ma9 rass

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
+
 /*
-*main function prints a string 
-*return 0 (Success)
+*main:function prints a string
+* return 0 (Success)
 */
 
 int main(void)
 {
 
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
+
 }
