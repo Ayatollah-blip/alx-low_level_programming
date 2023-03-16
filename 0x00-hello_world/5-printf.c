f37f4d2ce603463469ec6875d8633ea5eb75695e
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-03-17 00:44:32 +0100

    7ma9 tani with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 /**
 *5-printf-printline
+*main-print and return
+*return-0 successeful
 */
 
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,");
+printf ("with proper grammar, but the outcome is a piece of art, ");
 return(0);
 }
