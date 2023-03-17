262a007ea2518bd2b7fb0b9cb84671eb82fa165a
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-03-18 00:28:34 +0100

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,15 @@ int main(void)
 
 	for (i = 48 ; i < 58 ; i++)
 	{
-		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if( i != 57)
+		{
+			putchar(i);
+			putchar(',');
+			putchar(' ');
+		}
+		else
+			putchar(i);
 	}
-	putchar('\n');
+	
 return (0);
 }
