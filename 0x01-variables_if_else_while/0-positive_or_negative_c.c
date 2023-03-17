b84df866a1bdec7ed7cb9ddc8da3c6ce1d2f18c1
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-03-17 21:05:54 +0100

    Positif anything is better then negative anything

---
+++ 0x01-variables_if_else_while/0-positive_or_negative_c.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+  *main - A function that check if n positif, negative , or zero
+  *Dscription : bablabl
+  *Return: 0 (Success)
+  */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative", n);
+	return (0);
+}
