da1762a07b3e9511134818161e36f8c72852c01c
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-03-18 00:12:51 +0100

    alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - main function
+* description : print all the carcters in reverse
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int a = 122;
+
+	while (a > 97)
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+return (0);
+}
