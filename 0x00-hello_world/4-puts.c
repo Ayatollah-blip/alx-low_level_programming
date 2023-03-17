ca6a4d555f5b047445c979557f67e07d5fdada23
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-03-17 18:06:44 +0100

    first

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include<stdio.h>
-/*
-*description
+/**
 * main - A function prints a string
+*
 * Return: 0 (Success)
 */
 int main(void)
-/*Description for function main*/
 {
-/*
-*description
-* main - A function prints a string
-* Return: 0 (Success)
-*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 
