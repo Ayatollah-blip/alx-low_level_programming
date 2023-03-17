adff76443c6e7e2e3885f64dfe22e199123534b5
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-03-17 23:47:09 +0100

    loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - main program
+* Description : print from 0 to 9
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	for (int i = 0 ; i < 10 ; i++)
+	{
+		print("%d", i);
+	}
+	print("\n");
+return (0);
+}
