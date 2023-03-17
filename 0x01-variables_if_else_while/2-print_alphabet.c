90f34d9e277a48e3356d241d1a6628340ab5f277
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-03-17 23:01:29 +0100

    fucking letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - main function
+* description : print all the carcters
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int a = 97;
+
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+}
