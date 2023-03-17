856fb4b69b4fc96aa4404b5b24598873a84ea1aa
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-03-18 00:01:23 +0100

    numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - main function
+* Description: single numbers with putchar
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i);
+	}
+return (0);
+}
