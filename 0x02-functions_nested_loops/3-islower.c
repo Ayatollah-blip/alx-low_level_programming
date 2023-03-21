a855f7d4144d8ab037b92565b98707976ca43438
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-21 21:52:49 +0000

    lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+* _islower : function
+* Description: check for lower case
+*
+* Return: 0 (Success)
+*/
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else 
+		return (0);
+}
