69a322130454f87c4806e9351381adc7e89746d3
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-21 20:30:53 +0000

    proto

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,11 +11,4 @@ int main(void)
 {
 	print_alphabet();
 }
-void print_alphabet(void)
-{
-	int i;
-	for(i = 97 ; i < 123 ; i++)
-	{
-		_putchar(i);
-	}
-}
+
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+{
+	int i;
+	for(i = 97 ; i < 123 ; i++)
+	{
+		_putchar(i);
+	}
+}
