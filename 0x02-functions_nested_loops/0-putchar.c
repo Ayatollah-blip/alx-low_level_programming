aa94423a1769cc3a615da265dcbf8143872d6f82
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-21 19:18:59 +0000

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "_putchar.h"
+/**
+* main - this the main function
+* Description: printing putchar without print
+*
+* Return: 0 (Sucess)
+*/
+int main(void)
+{
+	_putchar();
+return (0);
+}
