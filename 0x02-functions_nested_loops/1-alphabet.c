40aaa502dcd4e16ba8724b9186797736e74c63cb
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-21 20:44:11 +0000

    mah

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
-* main - main function
+* print_alphabet -  function called to print alphabets in lower case
 * Description: call a function that prints lower case alphabets
 *
 * Return: 0 (Success)
 */
-#include "main.h"
-
 void print_alphabet(void)
 {
 	int i;
-	for(i = 97 ; i < 123 ; i++)
+
+	for (i = 97 ; i < 123 ; i++)
 	{
 		_putchar(i);
 	}
