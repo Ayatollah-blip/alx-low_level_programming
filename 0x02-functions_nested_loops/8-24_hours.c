fb7006a3e480d15c00cfc60babe85be018b6ca5f
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-21 23:30:27 +0000

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,18 +4,16 @@
 * jack_bauer - function
 * Description: primt time in minutes
 *
-* @c:  parametre caracter to be checked if lower case
 *
-* Return: 0 (Fail) / 1 (Success)
 *
 */
 void jack_bauer(void)
 {
-	int i, j , k, l;
+int i, j, k, l;
 
 	for (i = 0; i < 3 ; i++)
 	{
-		for (j = 0; j < 4 ; j++)
+		for (j = 0; j < 10 ; j++)
 		{
 			for (k = 0; k < 6 ; k++)
 			{
@@ -25,12 +23,11 @@ void jack_bauer(void)
 					_putchar(j + '0');
 					_putchar(':');
 					_putchar(k + '0');
-					_putchar(l + '0');	
+					_putchar(l + '0');
 					_putchar('\n');
 				}
-				
-			}	
-		}	
+			}
+		}
 	}
 _putchar('\n');
 }
