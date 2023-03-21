c79fc032b1da819d8507cd498b1826d6718ee2f6
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-21 22:22:39 +0000

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _abs - function
+* Description: return absolute value
+*
+* @c:  parametre caracter to be checked if lower case
+*
+* Return: int (absolute value)
+*
+*/
+int _abs(int c)
+{
+	return abs(c);
+}
