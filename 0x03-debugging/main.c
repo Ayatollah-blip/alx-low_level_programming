dac2b37d4aa5ac333aaf19c106f355425511485d
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-22 19:03:05 +0000

    first

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
+
+/**
+  *positive_or_negative - A function that check if n positif, negative , or zero
+  *Dscription : bablabl
+  *Return: 0 (Success)
+  */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+}
