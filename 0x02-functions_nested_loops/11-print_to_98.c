3e5e47e71ace3b929f38e0c00174fbb59fc89a45
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-22 01:51:22 +0000

    ca passe ya rb

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* print_to_98 - function
+* Description: print from n to 08
+*
+* @n:  parametre caracter to be started counting with
+*
+*
+*/
+void print_to_98(int n)
+{
+	int i, u, d;
+
+	for (i=n ; i < 99 ; i++)
+	{
+		if (i > 9)
+		{
+			u = i % 10;
+			d = i / 10;
+			_putchar(d + '0');
+			_putchar(u + '0');
+				
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+		if (i != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
