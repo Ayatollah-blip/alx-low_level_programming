7d0f3f2ecff5c9bdf6893baa9f6ee8d5833ec929
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-22 01:30:13 +0000

    rest

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,31 +11,30 @@ void times_table(void)
 {
 int i, j, m, d, u;
 
-	for (i = 0; i < 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j = 0; j < 10 ; j++)
+		for (j = 0 ; j < 10 ; j++)
 		{
-					m=i*j;
-					if (m > 9)
-					{
-						u = m % 10;
-						d = m / 10;
-						_putchar(44);
-						_putchar(32);
-						_putchar(d + '0');
-						_putchar(u + '0');	
-					}
-					else
-					{
-						if (j != 0)
-							{
-								_putchar(44);
-								_putchar(32);
-								_putchar(32);
-							}
-						_putchar(m + '0');
-					}
-				
+			m = i * j;
+			if (m > 9)
+			{
+				u = m % 10;
+				d = m / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+					_putchar(m + '0');
+			}
 		}
 		_putchar('\n');
 	}
