75f87467fc84791988e95fa2a7f69926529a820f
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-22 01:36:51 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* add - function
+* Description: add two input
+*
+* @a:  paramter 1 to be added
+* @b:  parameter 2 to be added
+*
+* Return: last digit (int)
+*
+*/
+int add(int a, int b)
+{
+	return (a+b);
+}
