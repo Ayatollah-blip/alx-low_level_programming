68cdb9945a0bd25d055ef86b7a76e8f438dafa95
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-22 01:21:09 +0000

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,10 +16,10 @@ int i, j, m, d, u;
 		for (j = 0; j < 10 ; j++)
 		{
 					m=i*j;
-					u = m % 10;
-					d = m / 10;
 					if (m > 9)
 					{
+						u = m % 10;
+						d = m / 10;
 						_putchar(44);
 						_putchar(32);
 						_putchar(d + '0');
@@ -27,10 +27,13 @@ int i, j, m, d, u;
 					}
 					else
 					{
+						if (j != 0)
+							{
+								_putchar(44);
+								_putchar(32);
+								_putchar(32);
+							}
 						_putchar(m + '0');
-							_putchar(44);
-							_putchar(32);
-							_putchar(32);
 					}
 				
 		}
