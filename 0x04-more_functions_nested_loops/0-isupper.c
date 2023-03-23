543fc02d0074cb36ba264ce93a17c674b9b3101d
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-23 17:40:37 +0000

    test uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,20 @@
 * _isupper - function
 * Description: function detects if the input is uppercase or not
 *
+* @c: function parameter caracter
+*
 * Return: 1 if uppercase and 0 otherwise
 */
 int _isupper(char c)
 {
 if (c > 64 && c < 91)
+{
+	_putchar('\n');	
 	return (1);
+}
 else
+{
+	_putchar('\n');
 	return (0);
+}
 }
