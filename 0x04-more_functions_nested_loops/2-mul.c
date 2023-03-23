2daa16b457a80eefca283187291be798b8e6c8d5
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-23 18:07:41 +0000

    test digit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* mul - function
+* Description: function multiply two parameters
+*
+* @a: function parameter caracter
+* @b: function parameter caracter
+*
+* Return: result
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
