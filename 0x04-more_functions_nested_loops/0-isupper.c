e7c98abf14c856f8cef1edd8a91ed68aad97124d
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-23 17:42:24 +0000

    test uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * _isupper - function
 * Description: function detects if the input is uppercase or not
@@ -10,13 +10,7 @@
 int _isupper(char c)
 {
 if (c > 64 && c < 91)
-{
-	_putchar('\n');	
 	return (1);
-}
 else
-{
-	_putchar('\n');
 	return (0);
-}
 }
