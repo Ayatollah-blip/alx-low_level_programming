1048fe85d3e1c9607ab96a29ec6b9e514bf6916a
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-25 16:49:43 +0000

    first test

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <ctype.h>
+/**
+* print_times_table - function
+* Description: times table
+*
+*
+*
+*/
+void print_times_table(int n)
+{
+int i, j, m, d, u;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		for (j = 0 ; j < n ; j++)
+		{
+			m = i * j;
+			if (m > 9)
+			{
+				u = m % 10;
+				d = m / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+					_putchar(m + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
