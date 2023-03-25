2ac050d99afc1f6b14a23b0fd2800f656ed4e34a
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-25 17:04:53 +0000

    first test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,14 +9,29 @@
 */
 void print_times_table(int n)
 {
-int i, j, m, d, u;
+int i, j, m, d, u, dd, c;
 
 	for (i = 0 ; i <= n ; i++)
 	{
 		for (j = 0 ; j <= n ; j++)
 		{
 			m = i * j;
-			if (m > 9)
+			if (m > 99)
+			{
+				u = m % 10;
+				d = m / 10;
+				dd = d % 10;
+				c = d / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				_putchar(c + '0');
+				_putchar(dd + '0');
+				_putchar(u + '0');
+	
+			}
+
+			else if (m > 9 && m < 100)
 			{
 				u = m % 10;
 				d = m / 10;
