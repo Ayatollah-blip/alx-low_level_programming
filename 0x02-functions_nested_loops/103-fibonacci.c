a511e3843b577034b30c5a0c753af10f8ece3772
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-25 22:50:22 +0000

    ex 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+* main - main function
+* Description: program that sums the multiple of 3 and 5 below 1024
+*
+* Return: 0 (Success)
+*/
+
+
+int main(void)
+{
+int i;
+long int  t1, t2, nextTerm, sum;
+
+sum = 0;
+t1 = 0;
+t2 = 1;
+nextTerm = t1 + t2;
+printf("%d", nextTerm);
+while (nextTerm < 4000000)
+{
+	t1 = t2;
+	t2 = nextTerm;
+	nextTerm = t1 + t2;
+	if ((nextTerm % 2) == 0)
+	{
+		sum = sum + nextTerm;
+	}
+}
+
+printf("%d\n",sum);
+return (0);
+}
