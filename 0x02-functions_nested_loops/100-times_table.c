6788d74607423c6b186f1886886b7a074f7734a3
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-25 17:25:40 +0000

    last ncha3alah

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,11 +11,11 @@ void print_times_table(int n)
 {
 int i, j, m, d, u, dd, c;
 
-if(!(n > 15) && !(n <= 0))
+if(!(n > 15) && !(n < 0))
 {
 	for (i = 0 ; i <= n ; i++)
 	{
-		for (j = 0 ; j < n ; j++)
+		for (j = 0 ; j <= n ; j++)
 		{
 			m = i * j;
 			if (m > 99)
