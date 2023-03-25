93dc97a5cb13be6a9434e197d8f91cd1e8b70dcd
Ayatollah <aymaneelyamani3@gmail.com>
2023-03-25 17:45:26 +0000

    last ncha3alah

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,7 @@
 * print_times_table - function
 * Description: times table
 *
-*
+* @n: input parameter
 *
 */
 void print_times_table(int n)
@@ -14,45 +14,31 @@ int i, j, m, d, u, dd, c;
 if (!(n > 15) && !(n < 0))
 {
 	for (i = 0 ; i <= n ; i++)
-	{
+{
 		for (j = 0 ; j <= n ; j++)
-		{
-			m = i * j;
+{			m = i * j;
+			u = m % 10;
+			d = m / 10;
 			if (m > 99)
-			{
-				u = m % 10;
-				d = m / 10;
-				dd = d % 10;
+{				dd = d % 10;
 				c = d / 10;
 				_putchar(44);
 				_putchar(32);
 				_putchar(c + '0');
 				_putchar(dd + '0');
-				_putchar(u + '0');
-			}
+				_putchar(u + '0');	}
 			else if (m > 9 && m < 100)
-			{
-				u = m % 10;
-				d = m / 10;
-				_putchar(44);
+{				_putchar(44);
 				_putchar(32);
 				_putchar(32);
 				_putchar(d + '0');
-				_putchar(u + '0');
-			}
+				_putchar(u + '0');	}
 			else
-			{
+{
 				if (j != 0)
-				{
-					_putchar(44);
+{					_putchar(44);
 					_putchar(32);
 					_putchar(32);
-					_putchar(32);
-				}
-					_putchar(m + '0');
-			}
-		}
-		_putchar('\n');
-	}
-}
-}
+					_putchar(32);	}
+					_putchar(m + '0');	}}
+		_putchar('\n');		}}}
