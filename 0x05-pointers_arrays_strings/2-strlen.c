14208cc2cfc7795d3d9b1320f7f804827beff371
Aymane <ayamaneelyamani3@gmail.com>
2023-03-27 22:00:28 +0000

    length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
-* _strlen - function
+* _strlen - function 
 * Description: calculate the length of a string
 *
-* @a: insert address that her value to be swaped
-* 
+* @s: insert address that her value to be started length sum from
+*
+* return: return length of a string
 */
 
 int _strlen(char *s)
 {
-	int p=0;
+	int p = 0;
 
 while (*s != '\0')
 {
 	s++;
-	p=p+1;
+	p = p + 1;
 }
 return (p);
 }
