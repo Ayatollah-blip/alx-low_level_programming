6210890ea42aacd2f865dfc2039e887b24c3e5da
Aymane <ayamaneelyamani3@gmail.com>
2023-03-28 02:24:02 +0000

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,11 +10,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
+int i = 0;
 while (*src != '\0')
 {
-	/*dest = *src;*/
+	*dest = *src;
 	dest++;
 	src++;
+	i++;
 }
-return (dest);
+return (dest - i);
 }
