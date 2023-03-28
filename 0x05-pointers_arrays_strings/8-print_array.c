93903ef2f614c77d3bbae4544df3f733d02fbe05
Aymane <ayamaneelyamani3@gmail.com>
2023-03-28 01:22:41 +0000

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_array - function
+* Description: print an array
+*
+* @a: insert address og the array
+* @n: araay length
+*/
+
+void print_array(int *a, int n)
+{
+int i;
+
+printf("%d", *a);
+
+for (i = 1 ; i < n ; i++)
+{
+printf(", %d", *(a + i));
+}
+}
