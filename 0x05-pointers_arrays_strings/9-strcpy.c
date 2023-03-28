41ce43f4df3f8e0cf4c70de20cb2d224c2a0e9eb
Aymane <ayamaneelyamani3@gmail.com>
2023-03-28 01:37:54 +0000

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* *_strcpy - function
+* Description: copy a string to a source
+*
+* @src: parameter that her value to be copied
+* @dest: parameter that her value to be copied from
+*
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+
+while (*src != '\0')
+{
+	*dest = *src;
+	src++;
+	dest++;
+}
+return (*dest);
+}
