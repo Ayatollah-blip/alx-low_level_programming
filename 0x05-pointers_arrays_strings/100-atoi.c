9d492c9da6ef109fa52d7f6c8d75dda79dd3c127
Aymane <ayamaneelyamani3@gmail.com>
2023-03-28 23:18:36 +0000

    test

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* *_strcpy - function
+* Description: copy a string to a source
+*
+* @src: parameter that her value to be copied
+* @dest: parameter that her value to be copied from
+*
+* Return:  address of the string to be printed
+*/
+
+int _atoi(char *s)
+{
+int sum = 0;
+
+while (*s != '\0')
+{
+	if (*s > 48 || *s < 57 || *s == 45)
+	{
+		sum = sum * 10 + (*s - 48);
+	}
+	s++;
+}
+
+return sum;
+}
