a566c8a31653f55ea6f267c20c7b66c742610022
Aymane <ayamaneelyamani3@gmail.com>
2023-03-29 22:12:03 +0000

    To upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* *string_toupper - function
+* Description: convert a string to a int
+*
+* @s: parameter that her value to be converted
+*
+* Return:  sum of the string to be printed
+*/
+
+int *string_toupper(char *s)
+{
+int sum = 0, i = 0;
+
+while (*s != '\0')
+{
+
+	if (*s < 123 && *s > 96)
+	{
+		*s = *s - 32;
+	}
+	s++;
+	s = s - i;
+}
+
+
+return (s);
+}
