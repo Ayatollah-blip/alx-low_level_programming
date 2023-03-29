2ea66966767ca72da1a43884da86168a4ccc9fd9
Aymane <ayamaneelyamani3@gmail.com>
2023-03-29 18:32:23 +0000

    test

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,24 @@
 #include "main.h"
 
+/**
+ * _strcat - function
+ * Description: concatenate two strings
+ * 
+ * Return: adress of 
+ * */
+
 char *_strcat(char *dest, char *src)
 {
 
-while (*dest != '\n')
+while (*dest)
 {
 	dest++;
 }
-
 while (*src != '\n')
 {
-	*dest = *src;
+	*dest  = *src;
 	dest++;
 	src++;
 }
 return (dest);
-
 }
