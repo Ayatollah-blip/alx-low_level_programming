dac1ed0fd299893ed962987dc369e887e64ccc84
Aymane <ayamaneelyamani3@gmail.com>
2023-03-29 18:51:11 +0000

    first concat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - function
+ * Description: concatenate two strings
+ *
+ * @dest: parameter to be returned
+ * @src: parameter to copy from
+ *
+ * Return: adress of
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j = 0;
+
+while (*dest != '\0')
+{
+	dest++;
+	i++;
+}
+while (j < n)
+{
+	*dest  = *src;
+	dest++;
+	src++;
+	i++;
+	j++;
+}
+
+dest = dest - i;
+
+return (dest);
+}
