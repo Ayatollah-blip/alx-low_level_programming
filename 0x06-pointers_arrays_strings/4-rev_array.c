02cda9e4a86c3617e94e2fc5b08c593a7d3df04e
Aymane <ayamaneelyamani3@gmail.com>
2023-03-29 22:02:27 +0000

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_array - function
+* Description: print an array
+*
+* @a: insert address og the array
+* @n: araay length
+*/
+
+void reverse_array(int *a, int n)
+{
+int i, constVar;
+if (n > 0)
+{
+	for (i = 0 ; i < n ; i++)
+	{
+		constVar = *(a + n - i);
+		*(a + n - i) = *(a + i);
+		*(a + i) = constVar;
+	}
+}
+}
