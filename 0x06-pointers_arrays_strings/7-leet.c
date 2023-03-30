f5cb56cdf57bb521552de3186e9432ee7efdb09b
Aymane <ayamaneelyamani3@gmail.com>
2023-03-30 00:15:05 +0000

    elite

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* *cap_string - function
+* Description: convert a string to a int
+* @s: parameter that her value to be converted
+* Return:  sum of the string to be printed
+*/
+char *cap_string(char *s)
+{
+int  i = 0;
+
+while (*s != '\0')
+{
+
+	if (*s < 123 && *s > 96)
+	{
+		if (*s == 'a' || *s == 'A')
+			*s = '4';
+		else if (*s == 'e' || *s == 'E')
+			*s = '3';
+		else if (*s == 'o' || *s == 'O')
+			*s = 0;
+		else if (*s == 't' || *s == 'T')
+			*s = 7;
+		else if (*s == 'l' || *s == 'L')
+			*s = 1;
+		else
+			*s = *s;
+	}
+	s++;
+	i++;
+}
+s = s - i;
+return (s);
+}
