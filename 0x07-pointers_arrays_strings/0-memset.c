29a1fec8ab72c58116148a2e406ce56256d3f8fc
Aymane <ayamaneelyamani3@gmail.com>
2023-04-03 21:05:17 +0000

    test

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,8 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i = 0;
-
+while (s != '\0' )
+{
 while (i < n)
 {
 	*s = b;
@@ -11,7 +12,7 @@ while (i < n)
 	i++;
 	printf("%c",*s);
 }
-
+}
 s = s - i;
 
 return (s);
