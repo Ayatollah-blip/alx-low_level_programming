b9b5e10046167749f7f1a1abff990145e82ab796
Aymane <ayamaneelyamani3@gmail.com>
2023-04-03 22:21:57 +0000

    str char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* _strchr - function
+* Description: mem set
+*
+* @s: string
+* @c: character
+*
+* Return: return string
+*/
+char *_strchr(char *s, char c)
+{
+unsigned int i = 0;
+char *pnt;
+
+while (*s != '\0')
+{
+	if (*s == b)
+		pnt = s;
+		break;
+	else
+		pnt = NULL;
+
+	s++;
+	i++;
+}
+
+return (pnt);
+}
