8e59069ad4c6db766d63176e667f3078779409ae
Aymane <ayamaneelyamani3@gmail.com>
2023-04-03 22:01:05 +0000

    copy memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i = 0;
+unsigned int i = 0;
 
 while (i < n && *src != '\0')
 {
@@ -27,6 +27,6 @@ while (i < n)
 	dest++;
 	i++;
 }
-}
+
 return (dest - i);
 }
