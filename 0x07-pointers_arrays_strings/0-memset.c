2fb389db24f59ee4688ffa707e14e4b85ca07773
Aymane <ayamaneelyamani3@gmail.com>
2023-04-03 21:27:38 +0000

    test

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,14 @@
 #include "main.h"
-
+/**
+* _memset - function
+* Description: mem set
+*
+* @s: string
+* @b: character
+* @n: int to be repeted
+*
+* Return: return string
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i = 0;
@@ -9,7 +18,7 @@ while (i < n)
 	*s = b;
 	s++;
 	i++;
-	printf("%c",*s);
+	printf("%c", *s);
 }
 
 s = s - i;
