a5935ea4d0ea3ad1bc482f1e149dbab854f97a3f
Aymane <ayamaneelyamani3@gmail.com>
2023-04-03 23:22:17 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* _strpbrk - function
+* Description: mem set
+*
+* @s: string
+* @accept: character
+*
+* Return: return string
+*/
+char *_strpbrk(char *s, char *accept)
+{
+char *pnt;
+
+while (*s != '\0')
+{
+	while (*accept != '\0')
+	{
+		if (*s == c)
+		{
+			pnt = s;
+			break;
+		}
+		accept++;
+		pnt = NULL;
+	}
+	s++;
+
+}
+
+return (pnt);
+}
