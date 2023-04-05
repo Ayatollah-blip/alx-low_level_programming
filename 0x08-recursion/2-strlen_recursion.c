ec72d8fe3631ea86e1029159e94246e5ce4592fd
Aymane <ayamaneelyamani3@gmail.com>
2023-04-05 22:43:06 +0000

    length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,15 +2,16 @@
 /**
 * _strlen_recursion - function
 *
-* @s: pointer of string to be printed
+* @s: pointer of string that we will count her length
 */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
+	int i;
 
 	if (*s == '\0')
 	{
-		i = 1; 
+	 	i = 0;
+		return (i);	
 	}
 	i = i + _strlen_recursion(s + 1);
 
