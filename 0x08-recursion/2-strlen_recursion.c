44c23d1513d337725bdb22f6d3bca735bb6ef2b7
Aymane <ayamaneelyamani3@gmail.com>
2023-04-05 22:26:37 +0000

    length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strlen_recursion - function
+*
+* @s: pointer of string to be printed
+*/
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	{
+		i = 1
+		return i;
+	}
+	_strlen_recursion(s + 1);
+	i = i + 1;
+ 
+}
