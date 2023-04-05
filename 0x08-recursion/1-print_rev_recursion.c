e3f9f00681222dac1b134bce6a9b459d807e338d
Aymane <ayamaneelyamani3@gmail.com>
2023-04-05 21:00:37 +0000

    print string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 *  _print_rev_recursion - function
-* Description: print string in reverse 
-* 
+* Description: print string in reverse
+*
 * @s: pointer of string to be printed in reverse
 */
 void _print_rev_recursion(char *s)
