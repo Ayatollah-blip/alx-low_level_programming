c905c8a4b93fa99ca3de866f8c420a0446c59b5f
Aymane <ayamaneelyamani3@gmail.com>
2023-04-05 22:36:38 +0000

    length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,14 +6,13 @@
 */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
+	int i;
 
 	if (*s == '\0')
 	{
-		i = 1;
-		return i;
+		i = 1 
 	}
-	_strlen_recursion(s + 1);
-	i = i + 1;
- 
+	i = i + _strlen_recursion(s + 1);
+
+return (i);
 }
