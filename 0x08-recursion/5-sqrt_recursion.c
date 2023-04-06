73cec08aac7f88ba3bef0ecb90c86d6f9fb62ff0
Aymane <ayamaneelyamani3@gmail.com>
2023-04-06 01:44:55 +0000

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,23 +12,25 @@ int _sqrt(int n, int i);
 */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n,1));
+	return (_sqrt(n, 1));
 }
 /**
+*_sqrt - function that calculate the square of n base on i
 *
+*@n: number whom the square needed to be calculated
+*@i: number to start from calculating the square
 *
-*
-*
+*Return: square of n
 */
 int _sqrt(int n, int i)
 {
 	int square = i * i;
-	
+
 	if (square > n)
 		return (-1);
 
 	else if (square == n)
 		return (i);
 	else
-		return (_sqrt(n, i+1));
+		return (_sqrt(n, i + 1));
 }
