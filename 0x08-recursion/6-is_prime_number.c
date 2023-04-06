a3eb915ee83b1ed5ff13bb9ed12651946528a4e4
Aymane <ayamaneelyamani3@gmail.com>
2023-04-06 02:02:55 +0000

    test prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* is_prime_number - function that test if prime number
+*
+* @n: the number to be test if prime number.
+*
+* Return: result
+*/
+int is_prime_number(int n)
+{
+
+	return (prime_number(n, 2));
+}
+/**
+*prime_number - function that calculate the square of n base on i
+*
+*@n: number whom to be tested if prime number
+*@i: number to start testing from
+*
+*Return: square of n
+*/
+int prime_number(int n, int i)
+{
+	int num = n % i;
+
+	if (num ==  0)
+		return(0);
+
+	else if (num == n)
+		return (1);
+	else
+		return (prime_number(n, i + 1));
+}
