6c054259cd1b346245cda6c2ddff77795083e291
Aymane <ayamaneelyamani3@gmail.com>
2023-04-06 00:27:14 +0000

    test

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,13 +11,13 @@ int _sqrt_recursion(int n)
 	int *j;
 	int i = n / 2;
 
-	*j = 0;
 	if (n < 0)
 		return (-1);
 	else if (n == *j)
 		return (n);
 	else
-	{	*j = i;
+	{	
+		j = &i;
 		i = (i + n/i)/2;
 
 		return _sqrt_recursion(i);
