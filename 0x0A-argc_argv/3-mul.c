75bc9cd1040084431f284cd592e44d84e190d6ef
Aymane <ayamaneelyamani3@gmail.com>
2023-04-10 01:43:06 +0000

    zb

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,42 +1,53 @@
-#include<stdio.h>
-/**
-* *_atoi - function
-* Description: convert a string to a int
-*
-* @s: parameter that her value to be converted
-*
-* Return:  sum of the string to be printed
-*/
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
 int _atoi(char *s)
 {
-int sum = 0, i = 1;
+	int i, d, n, len, f, digit;
 
-while (*s != '\0')
-{
-	if (*s == 45)
-		i = i * -1;
-	else if (*s == 43)
-		i = i * 1;
-	else
-		i = i;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	if (*s < 58 && *s > 47)
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		if (i < 0)
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			sum = i * (sum * 10 + (*s - 48));
-			i = 1;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		else
-			sum = i * (sum * 10 + (*s - 48));
+		i++;
 	}
-	s++;
+	if (f == 0)
+		return (0);
+
+
+	return (n);
+
 }
 
 
-return (sum);
-}
 /**
 * main - program
 * Description: function that prints the number of argc
@@ -52,7 +63,7 @@ int arg1, arg2, num;
 
 arg1 = _atoi(argv[1]);
 arg2 = _atoi(argv[2]);
-num = arg1 * arg2;
+num = arg1 *arg2;
 (void) argc;
 if (argv[1] != NULL && argv[2] != NULL)
 {
