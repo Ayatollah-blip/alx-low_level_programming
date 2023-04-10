550be519797a89ea27352c76bfdf10dcd5f244f3
Aymane <ayamaneelyamani3@gmail.com>
2023-04-10 01:04:34 +0000

    test

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+* main - program
+* Description: function that prints all the argv
+*
+* @argv: vector of string of command line arguments
+* @argc: integer number that count the command line arguments
+*
+* Return: 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0 ; i < argc ; i++)
+	printf("%s\n", argv[i]);
+
+return (0);
+}
