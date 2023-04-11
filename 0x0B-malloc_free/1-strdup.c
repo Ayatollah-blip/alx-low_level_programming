04a479bf7f11bbcd2a67978701dba0f725a83282
Aymane <ayamaneelyamani3@gmail.com>
2023-04-11 22:23:08 +0000

    test copy string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* _strdup - function
+* Description: function that copies a string
+*
+* @str: parameter that represent the string of the array
+*
+* Return: NULL if size is 0 or pointer if not
+*/
+char *_strdup(char *str)
+{
+char *a;
+unsigned int i = 0, j = 0;
+
+while (*str == '\0')
+{
+	str++;
+	i++;
+}
+str = str - i;
+
+a = malloc(i * sizeof(char));
+
+if (a == NULL)
+	return (NULL);
+while (j < i)
+{
+	a[j] = str[j];
+	j++;
+}
+}
+return (a);
+}
