f4863abf23f9db5b6b9bed900e62e96042cc2757
Aymane <ayamaneelyamani3@gmail.com>
2023-04-11 21:52:52 +0000

    test

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,6 +5,8 @@
 *
 * @size: parameter that represent the size of the array
 * @c: parameter that represent the caracter to be printed
+*
+* Return: NULL if size is 0 or pointer if not
 */
 char *create_array(unsigned int size, char c)
 {
@@ -16,9 +18,12 @@ if (size == 0)
 else
 {
 	a = malloc(size * sizeof(char));
+	if (a == NULL)
+		return (NULL);
 	while (i < size)
 	{
 		a[i] = c;
+		i++;
 	}
 }
 return (a);
