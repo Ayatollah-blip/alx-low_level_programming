e5b002e0595e2fdd8dc59bee5b313a339e87bfdd
Aymane <ayamaneelyamani3@gmail.com>
2023-04-12 00:38:22 +0000

    2 diemsion array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* str_concat - function
+* Description: function that concatenate two string
+*
+* @width: parameter that represent the width of the two domension array
+* @height: parameter that represent the height of the two diemension array
+*
+* Return: NULL if size is 0 or pointer if not
+*/
+char **alloc_grid(int width, int height)
+{
+char *a;
+int i = 0, j;
+
+if (width == NULL)
+	return NULL;
+if (height == NULL)
+	return NULL;
+a = malloc(width * height * sizeof(char));
+if (a == NULL)
+	return (NULL);
+
+while (i < height)
+{
+	j = 0;
+	while (j < width)
+	{
+		a[i][j] = 0;
+		j++;
+	}
+	i++;
+}
+
+return (a);
+}
