39c218dde36d5087a051375219ed66010cc94c58
Aymane <ayamaneelyamani3@gmail.com>
2023-04-12 01:18:35 +0000

    2 diemsion array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,12 +21,7 @@ while (j < height)
 {
 	a[j] = malloc(width * sizeof(int));
 	if (a[j] == NULL)
-	{
-		for(;j >= 0 ;j--)
-			free(a[j]);
-		free(a);
 		return (NULL);
-	}
 	j++;
 }
 while (j <  height)
