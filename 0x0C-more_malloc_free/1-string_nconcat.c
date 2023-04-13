25612f184f79e4ce0db3320168db20e9421725f1
Aymane <ayamaneelyamani3@gmail.com>
2023-04-13 22:56:10 +0000

    test n concatenate

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+* string_nconcat - function
+* Description: function that concatenate two string
+*
+* @s1: parameter that represent the string
+* @s2: parameter that represent the string
+* @n : number of caraters in string 2 to be copied
+*
+* Return: NULL if size is 0 or pointer if not
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *a;
+int len1, len2, len_str;
+int i = 0, j;
+
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
+len1 = strlen(s1);
+len2 = strlen(s2);
+len_str = len1 + n;
+a = malloc((len_str + 1) * sizeof(char));
+if (a == NULL)
+	return (NULL);
+
+while (i < len1)
+{
+	a[i] = s1[i];
+	i++;
+}
+j = len1;
+i = 0;
+while (i < n)
+{
+	a[j] = s2[i];
+	j++;
+	i++;
+}
+
+return (a);
+}
