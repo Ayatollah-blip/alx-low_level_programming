15730256ee54413f23b50fd7d803a90679988643
Aymane <ayamaneelyamani3@gmail.com>
2023-04-13 23:48:18 +0000

    test calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _calloc - Function
+* Description: function calloc base on malloc
+*
+* @nmemb: parameter for number to be alocated
+* @size: parameter for the size
+*
+* Return: pointer if success
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *ptr;
+
+if (nmemb == 0 || size == 0)
+	return (NULL);
+ptr = malloc(b * sizeof(size));
+if (ptr == NULL)
+{
+	return (NULL);
+}
+return (ptr);
+}
