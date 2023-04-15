c2535a4c87a117291ec5aa64f382e6fea9083b53
Aymane <ayamaneelyamani3@gmail.com>
2023-04-15 00:42:28 +0000

    test

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - converts a string to an integer
+ * @argc: count 
+ *
+ * Return: the int converted from the string
+ */
+int main(int argc, char *argv[])
+{
+
+int i, number,sum = 0;
+char arg;
+
+if (argc == 1)
+	printf("0\n");
+
+for (i = 1 ; i < argc ; i++)
+{
+	number = atoi(argv[i]);
+	if (number > 0)
+	{
+		sum = sum + number;
+	}
+	else if (number == 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
+printf("%d\n",sum);
+}
