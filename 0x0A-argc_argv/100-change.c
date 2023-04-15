94b0a5b176ef9a1899ccaa597fa3a524408a9bcf
Aymane <ayamaneelyamani3@gmail.com>
2023-04-15 02:26:03 +0000

    test cemt

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - converts a string to an integer
+ * @argc: count the argument input
+ * @argv: table of arguments inputs
+ *
+ * Return: the int converted from the string
+ */
+int main(int argc, char *argv[])
+{
+
+int i, number, sum = 0;
+int tab[4]={25, 10, 2, 1};
+
+if (argc != 2)
+{
+	printf("Error\n");
+	return (1);
+}
+number = atoi(argv[1]);
+for (i = 0 ; i < 4 ; i++)
+{
+	if (number >= tab[i])
+	{
+		sum = sum + (number / tab[i]);
+		number = number % tab[i];
+	}
+}
+
+printf("%d\n", sum);
+
+return (0);
+}
