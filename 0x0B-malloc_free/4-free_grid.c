a0c506dcad41007302b28e9efc87f69e0dad1fcf
Aymane <ayamaneelyamani3@gmail.com>
2023-04-15 22:51:29 +0000

    free array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* alloc_grid - function
+* Description: function that concatenate two string
+*
+* @width: parameter that represent the width of the two domension array
+* @height: parameter that represent the height of the two diemension array
+*
+* Return: NULL if size is 0 or pointer if not
+*/
+void free_grid(int **grid, int height)
+{
+int i = 0, j = 0;
+
+while (j < height)
+{
+	for (i = 0 ; i < width ; i++)
+		free(a[i]);
+	free(a);
+	j++;
+}
+}
