42a451712e29acb663102596343b1aeb1f26bdb5
Aymane <ayamaneelyamani3@gmail.com>
2023-04-17 23:19:10 +0000

    typedef structure

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+/**
+* new_dog - function
+* description: function that create a struct
+*
+* @name: pointer of a caracter
+* @age: parameter of age
+* @owner: pointer
+*
+* Return: return structure Hmar
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t Hmar;
+
+	Hmar.name = name;
+	Hmar.age = age;
+	Hmar.owner = owner;
+
+return (Hmar);
+}
