e9f94510f485791bffc83adf4d532d32cc9ec59e
Aymane <ayamaneelyamani3@gmail.com>
2023-04-18 00:00:38 +0000

    create struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,15 +11,24 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int len1, len2;
 	dog_t *Hmar;
 
+	len1 = strlen(name) + 1;
+	len2 = strlen(owner) + 1;
 	Hmar = malloc(sizeof(dog_t));
-
 	if (Hmar == NULL)
 		return NULL;
-	(*Hmar).name = name;
+	(*Hmar).name = malloc(sizeof(char) * len1);
+	if ((*Hmar).name == NULL)
+		return NULL;
+	(*Hmar).owner = malloc(sizeof(char) * len2);
+	if ((*Hmar).owner == NULL)
+		return NULL;
+	
+	strcpy((*Hmar).name, name);
 	(*Hmar).age = age;
-	(*Hmar).owner = owner;
+	strcpy((*Hmar).owner, owner);
 
 return (Hmar);
 }
