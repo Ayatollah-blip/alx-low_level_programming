643ca3ac83a5ab0323fd79a7098f8c64ea5d44c0
Aymane <ayamaneelyamani3@gmail.com>
2023-04-20 00:41:10 +0000

    pointer function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+* print_name - function that calls a function pointer to print 
+*
+* @name: parameter that get the name to be printed
+*
+* @f: parameter function to be called
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+f(*name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,4 @@
+
+
+
+This is function of pointer learning
