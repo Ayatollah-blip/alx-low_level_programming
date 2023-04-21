a8ac58ac64965ac1353b60ec18621b989b283a51
Aymane <ayamaneelyamani3@gmail.com>
2023-04-21 00:26:38 +0000

    print args

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+* print_numbers - function that prints all arguments
+*
+* @n: number of argumets to be puted
+* @separator: constant to seperate the args while printing
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list ap;
+unsigned int i;
+
+
+va_start(ap, n);
+printf("%d", va_arg(ap, int));
+for (i = 1 ; i < n ; i++)
+{
+	printf("%c %d", *separator, va_arg(ap, int));
+}
+printf("\n");
+va_end(ap);
+
+}
