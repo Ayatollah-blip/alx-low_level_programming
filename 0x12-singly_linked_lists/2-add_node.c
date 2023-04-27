49c75d4d898df0bde9ba627883e5341256e520f9
Aymane <ayamaneelyamani3@gmail.com>
2023-04-27 23:21:59 +0000

    test add node single list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,9 +10,10 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *temp;
+unsigned int len = 0;
 
-	while (str[len])
-		len++;
+while (str[len])
+	len++;
 temp = malloc(sizeof(list_t));
 if (!temp)
 	return (NULL);
