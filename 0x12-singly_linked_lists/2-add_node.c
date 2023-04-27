024680e9b378a7d0214b3a4d323defb69e23674e
Aymane <ayamaneelyamani3@gmail.com>
2023-04-27 23:17:00 +0000

    test

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+* add_node - function that prints all the elements of a list_t
+*
+* @head: pointer of a linked lists
+* @str: string to be place in node
+*
+* Return: number of elements of a list_t
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *temp;
+
+	while (str[len])
+		len++;
+temp = malloc(sizeof(list_t));
+if (!temp)
+	return (NULL);
+
+temp->str = strdup(str);
+temp->len = len;
+temp->next = *head;
+*head = temp;
+
+return (temp);
+}
