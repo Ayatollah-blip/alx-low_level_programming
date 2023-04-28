515f375af1f1d443be47b4cabcebff4aa7adfdb7
Aymane <ayamaneelyamani3@gmail.com>
2023-04-28 21:30:24 +0000

    test .asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,15 @@
-SECTION .data		;initialized DATA
-
-msg: db `Hello, Holbrton\n`,0
-
-
 global main
 extern printf
 
 SECTION .text		;asm code
 
 main:
-	push rbp
-	mov rbp, rsp
-	sub rsp, 16
 	mov edi, msg
-	xor eax,eax
+	xor eax, eax
 	call printf
 	mov eax, 0
-
-	add rsp, 16
-	leave
 	ret
+
+SECTION .data		;initialized DATA
+
+msg: db `Hello, Holbrton\n`,0
