d6479b4915dc0c83f2022400d966ff981d719e43
Aymane <ayamaneelyamani3@gmail.com>
2023-04-30 16:21:24 +0100

    print linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+/**
+* print_listint - finction that prints a integer from linked list
+*
+* @h: this the linked list to be printed
+*
+* Return: the number of variable composed of  linked lists
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t i = 0;
+
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+
+return (i);
+}
+
