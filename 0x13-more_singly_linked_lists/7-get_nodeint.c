ede36f1ee2003dc5e664ebc8e02f1dc95f36a6af
Aymane <ayamaneelyamani3@gmail.com>
2023-04-30 20:55:10 +0100

    test get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,15 +10,16 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 
 listint_t *node;
-unsigned int i;
+unsigned int i = 0;
 
 if (!head)
 	return (NULL);
-for (i = 0 ; i < index ; i++)
+
+while (head && i < index)
 {
 	head = (head)->next;
 	node = head;
-
+	i++;
 }
 return (node);
 }
