049acc972adfc7e4311b7df4968e70fee91c71f0
Aymane <ayamaneelyamani3@gmail.com>
2023-04-30 17:20:16 +0100

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,11 +9,17 @@
 */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t node;
+
+listint_t *node;
+
+node = malloc(sizeof(listint_t));
+if (!node)
+	return (NULL);
 
 node->n = n;
 node->next = *head;
+*head = node;
 
-return (node);
+return (*head);
 }
 
