20debb6e608548ce222548487ee2ef8148e6f9d1
Aymane <ayamaneelyamani3@gmail.com>
2023-04-30 21:00:32 +0100

    test get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 /**
-* pop_listint - finction that pop a integer from linked list
+* get_nodeint_at_index - finction that get an element from linked list
 *
 * @head: this the linked list to be printed
+* @index: the index where the element you want to be fetched
 *
-* Return: the number to be poped
+* Return: the element you need
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
