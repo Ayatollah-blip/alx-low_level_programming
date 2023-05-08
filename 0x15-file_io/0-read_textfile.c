cedd7c64bf41b63840b75c8dce8853de25c9fdc2
Aymane <ayamaneelyamani3@gmail.com>
2023-05-08 23:48:44 +0100

    text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
 /**
-*read_text - read and write number text from file
+* read_textfile - read and write number text from file
 *
 * @filename: name of the file
 * @letters: number of the elements to be read from the file
@@ -10,7 +12,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char buffer;
+char *buffer;
 ssize_t fp;
 ssize_t w;
 ssize_t r;
@@ -19,9 +21,10 @@ ssize_t r;
 fp = open(filename, O_RDONLY);
 if (fp == -1)
 	return (0);
-buffer = malloc(sizeof(letters));
-r = read(filename, buffer, letters);
-w = write(stdout_FileNo,buffer,r);
+buffer = malloc(sizeof(char) * letters);
+
+r = read(fp, buffer, letters);
+w = write(STDOUT_FILENO, buffer, r);
 
 free(buffer);
 close(fp);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN
 #define MAIN
 
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 #include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
