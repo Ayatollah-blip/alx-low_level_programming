fe5c84d5fd1e988646fe2a5342c975c983d41835
Aymane <ayamaneelyamani3@gmail.com>
2023-05-09 00:21:57 +0100

    prep

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,9 +19,8 @@ if (filename == NULL)
 	return (-1);
 if (text_content != NULL)
 {
-while (text_content[len])
-{
-len++;
+for (len = 0 ; text_content[len];)
+	len++;
 }
 }
 fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
