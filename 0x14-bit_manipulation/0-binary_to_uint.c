ea26bb698734b9ef424697e2df72e9874e3272dd
Aymane <ayamaneelyamani3@gmail.com>
2023-07-06 21:50:15 +0100

    test

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+* bower - power function
+*
+* @n: power degree
+*
+* Return: the result of power function
+*/
+unsigned int bower(int n)
+{
+unsigned int bow = 1;
+
+while (n > 0)
+{
+	bow = bow * 2;
+/*	printf("   Power :%d \n", bow);*/
+	n--;
+}
+
+return (bow);
+}
+
+/**
+* binary_to_uint - function that convert binary to uint
+*
+* @b: pointer of a string
+*
+* Return: return the binary converted to uint
+*/
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int a, sum = 0;
+int i, len;
+
+
+len = strlen(b);
+
+for (i = 0 ; i < len ; i++)
+{
+if (b[len - i - 1] == '0')
+{
+	a = 0;
+}
+else if (b[len - i - 1] == '1')
+{
+	a = 1;
+}
+else
+{
+	return (0);
+}
+
+if (a == 1)
+{
+	sum = sum + bower(i);
+}
+}
+return (sum);
+}
+
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* clear_bit - function that clears a bit based on index
+*
+* @n: number that we need to get from him the bit we need
+* @index: the index we want take the value of the bit
+*
+* Return: return the bit we need
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int *bit = n;
+
+
+if (index > 63)
+	return (-1);
+else
+{
+	*bit = (~(1UL << index) & *n);
+	return (1);
+}
+}
+
