05c57cb3c2e13fa219664d2075f978284a12acf3
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-17 22:06:51 +0100

    add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,14 +4,13 @@
 #include "lists.h"
 
 /**
-* dlistint_len - function that return length of linkd list
+* add_dnodeint - function that return length of linkd list
 * Description: chakhbat lakhabit
 * @h : pointer of the head of the list to be printed
 * Return: number of nodes
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t new_node = {n, NULL, NULL};
 	dlistint_t *buffer;
 
 	buffer = malloc(sizeof(dlistint_t));
@@ -20,6 +19,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	
 	buffer->n = n;
 	buffer -> next = *head;
-	head = &buffer;
+	*head = buffer;
 	return (*head);
 }
