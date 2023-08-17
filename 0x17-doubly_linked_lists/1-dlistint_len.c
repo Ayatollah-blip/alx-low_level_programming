8b8775d88f197a420057ae44c983b2007d48b2e1
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-17 21:15:16 +0100

    test

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,13 +9,12 @@
 * @h : pointer of the head of the list to be printed
 * Return: number of nodes
 */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
