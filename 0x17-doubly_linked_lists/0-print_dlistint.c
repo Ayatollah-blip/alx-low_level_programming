ca7d2f9ad2763c3af12acb0cf10f64db1e5839b1
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-17 21:05:38 +0100

    test

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* print_dlistint - function that prints doubly linked list
+* Description: chakhbat lakhabit
+* @h : pointer of the head of the list to be printed
+* Return: number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d \n", h->n);
+		h = h->next;
+		i++;
+	}
+return (i);
+}
---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef LISTS
+#define LISTS
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+
+typedef struct dlistint_t
+{
+	int n;
+	struct dlistint_t *next;
+	struct dlistint_t *prev;
+}dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
