634b3c7f04bfee9a853df3cf5c12fe5d06ca6099
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-21 14:36:05 +0100

    first

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stdlib.h>
-#include <stddef.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -23,7 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-
 
 #endif
