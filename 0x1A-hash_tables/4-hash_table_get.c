2212100760ed05db01b4bc0e08233b78aecf753e
aymane el yamani <aymaneelyamani3@gmail.com>
2023-09-09 02:39:50 +0100

    test

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_get - Retrieve the value associated with
  *                  a key in a hash table.
@@ -11,20 +12,19 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-hash_node_t *node_get, *buff;
+hash_node_t *node;
+	unsigned long int index;
 
-id = key_index((unsigned char *)key, ht->size);
-buff = malloc(sizeof(node_get));
-node_get = malloc(sizeof(node_get));
-if (strcmp(ht->array[id]->key, key) == 0)
-	{
-	buff->key = key;
-	buff->value = ht->array[id]->value;
-	buff->next = node_get;
-	node_get = buff;
-	return (node_get);
-	}
-else
-	return (NULL);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
 }
