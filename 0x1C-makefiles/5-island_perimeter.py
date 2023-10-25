647adc86d4590b97bb3cf5ea0dbb620c0a4ecff3
aymane el yamani <aymaneelyamani3@gmail.com>
2023-10-25 22:16:27 +0100

    te

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
+""" Return the perimeter of the island """
+
 
 def island_perimeter(grid):
+    """ Returns the parameter of the island
+    Args:
+        grid - list : list of list of integers
+    Return:
+        The parameter of an island
+    """
+
     if not grid:
         return 0
-    column, row = len(grid), len(grid[0])
     count = 0
-    for gri in grid:
-        for gr in gri:
-            if gr == 1:
-                count += 1 
+    rows, columns = len(grid), len(grid[0])
+    for i in range(rows):
+        for j in range(columns):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    count += 1
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    count += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    count += 1
+                if j == columns - 1 or grid[i][j + 1] == 0:
+                    count += 1
+    return count
